08de43108e3951da88fa923593c7e424d626a61d
pratikbaid3 <pratikbaid3@gmail.com>
2020-12-10 17:07:16 +0530

    feat: Partially completed getting trophy guide

--- ps4_guide_scraper.py
+++ ps4_guide_scraper.py
@@ -14,10 +14,15 @@ game_name=[]
 game_guide_url=[]
 
 #Trophy Name
+trophy_name_list=[]
 #Trophy Image
+trophy_img_list=[]
 #Trophy Type
+trophy_type_list=[]
 #Trophy Description
+trophy_description_list=[]
 #Trophy Guide
+trophy_guide_list=[]
 
 for i in gameData:
     game_name.append(i[0])
@@ -30,7 +35,40 @@ try:
     page_html=uClient.read()
     uClient.close()
     page_soup=soup(page_html,"html.parser")
-    trophy_list=page_soup.find_all('li',{'class':'achilist__item'})
-    print(len(trophy_list))
+    #Trophy Image
+    trophy_img_list=page_soup.find_all('div',{'class':'list__pic'})
+    trophy_img_list=[img.find('img')['data-src'] for img in trophy_img_list]
+    #Trophy Name
+    trophy_name_list=page_soup.find_all('div',{'class':'achilist__header'})
+    trophy_name_list=[name.find('a').find('h4').text for name in trophy_name_list]
+    #Trophy Description
+    trophy_description_list=page_soup.find_all('div',{'class':'achilist__data'})
+    trophy_description_list=[description.find('p',recursive=False).text for description in trophy_description_list]
+    #Trophy Type
+    trophy_type_list=page_soup.find_all('span',{'class':'achilist__value-numb'})
+    trophy_type_list=[trophy.find('img')['src'] for trophy in trophy_type_list]
+    trophy=[]
+    for i in trophy_type_list:
+        if(i=='/images/icons/trophy_gold.png'):
+            trophy.append('GOLD')
+        elif(i=='/images/icons/trophy_silver.png'):
+            trophy.append('SILVER')
+        elif(i=='/images/icons/trophy_bronze.png'):
+            trophy.append('BRONZE')
+        elif(i=='/images/icons/trophy_platinum.png'):
+            trophy.append('PLATINUM')
+    trophy_type_list=trophy
+    #Trophy Guide
+    trophy_guide_list=page_soup.find_all('div',{'class':'mt-1 pl-1'})
+    trophy_guide_list=[trophy.find('div',recursive=False) for trophy in trophy_guide_list]
+
+    
+
+
+
+    print(trophy_guide_list[3])
+
+
+
 except Exception as e:
     print(e)
